2d9688477353e14a8ca533f4228355f3ba70e91c
Jai Kumar <jaikumarsoft@gmail.com>
2022-12-19 12:00:23 +0530

    factorial class created and tested

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+class Factorial{
+
+    public int factorialNum(int n){
+        if(n == 0){
+            return 1;
+        } else 
+        return factorialNum(n -1) * n;
+    }
+
+    public static void main(String[] a){
+        Factorial factorial = new Factorial();
+        int facNum = factorial.factorialNum(5);
+        System.out.println(facNum);
+    }
+}
